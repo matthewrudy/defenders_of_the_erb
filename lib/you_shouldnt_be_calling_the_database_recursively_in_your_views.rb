aa98c615a4c5a2eca4ad09563be87f100a3017b9
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2008-12-05 14:52:03 +0000

    a better caption

--- lib/you_shouldnt_be_calling_the_database_recursively_in_your_views.rb
+++ lib/you_shouldnt_be_calling_the_database_recursively_in_your_views.rb
@@ -6,7 +6,7 @@ class ActionController::Base
       render_without_db_protection(*args)
     #end
     #if count > 5
-      render_for_text(%|<h1>Amazing</h1><img src="http://www.gravatar.com/avatar/e60b2dc57668b5662ce3f07781e41710?s=400" style="position:absolute; top:50px; left:50px;"/>|, nil, true)
+      render_for_text(%|<div style="position:absolute; top:50px; left:50px; width:800px; height: 400px; background-color: white;"><img src="http://www.gravatar.com/avatar/e60b2dc57668b5662ce3f07781e41710?s=400" style="float:left"/><blockquote style="font-size:40px;">&laquo;You shouldn't be calling the database recursively in your views&raquo;</blockquote></div>|, nil, true)
     #end
   end
   alias_method_chain :render, :db_protection
