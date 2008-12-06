64473896ef3371c001e5dbf020e2e621b730a30a
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2008-12-06 17:58:45 +0000

    log the queries in full, and REALLY ignore cached blocks in erb templates

--- lib/you_shouldnt_be_calling_the_database_recursively_in_your_views.rb
+++ lib/you_shouldnt_be_calling_the_database_recursively_in_your_views.rb
@@ -1,18 +1,33 @@
-$queries_executed = 0
+$queries_executed = []
 
 # YouShouldntBeCallingTheDatabaseRecursivelyInYourViews
 require 'action_controller/base'
 class ActionController::Base
   def render_with_db_protection(*args)
-    count_before = $queries_executed
+    $queries_executed = []
     render_without_db_protection(*args)
-    count =  $queries_executed - count_before
-    if count > 0
-      render_for_text(%|<div style="position:absolute; top:50px; left:50px; width:800px; height: 400px; background-color: white;"><img src="#{matthewrudy_url}" style="float:left"/><blockquote style="font-size:40px;">&laquo;You shouldn't be calling the database recursively in your views&raquo;</blockquote></div>|, nil, true)
+    queries_executed = $queries_executed.dup
+    queries_count =  queries_executed.length
+    if queries_count > 0
+      alert_html = <<-HTML
+        <div style="position:absolute; top:50px; left:50px; width:800px; height: 400px; background-color: white; border: 1px solid black">
+          <img src="#{matthewrudy_url}" style="float:left"/>
+          <blockquote style="font-size:40px;">
+            &laquo;You shouldn't be calling the database recursively in your views&raquo;
+          </blockquote>
+          <blockquote style="font-size:20px;">
+            You called the database #{queries_count} times, that's way too many. (<a href="#" onclick="document.getElementById('matthewrudy-queries-list').style.display = 'block'; return false;">show</a>)</p>
+          </blockquote>
+          <ul id="matthewrudy-queries-list" style="display:none">
+            #{queries_executed.map{|query| "<li>#{query.inspect}</li>"}.join("\n")}
+          </ul>
+        </div>
+      HTML
+      render_for_text(alert_html, nil, true)
     end
   end
   alias_method_chain :render, :db_protection
-  
+
   def matthewrudy_url
     File.exist?(File.join(Rails.root, "public", "matthewrudy.jpg")) ? "/matthewrudy.jpg" : "http://www.gravatar.com/avatar/e60b2dc57668b5662ce3f07781e41710?s=400"
   end
@@ -24,18 +39,18 @@ ActiveRecord::Base.connection.class.class_eval do
 
   def execute_with_query_record(sql, name = nil, &block)
     unless self.stop_recording_queries
-      $queries_executed +=1 unless IGNORED_SQL.any? { |r| sql =~ r }
+      $queries_executed << sql unless IGNORED_SQL.any? { |r| sql =~ r }
     end
     execute_without_query_record(sql, name, &block)
   end
-
   alias_method_chain :execute, :query_record
-  
+
   attr_accessor :stop_recording_queries
   def without_query_record
     state_before, self.stop_recording_queries = self.stop_recording_queries, true
-    return yield
+    rtn = yield
     self.stop_recording_queries = state_before
+    return rtn
   end
 end
 
@@ -43,8 +58,18 @@ require 'active_support/cache'
 class ActiveSupport::Cache::Store
   def fetch_with_query_record_off(*args, &block)
     ActiveRecord::Base.connection.without_query_record do
-      return fetch_without_query_record_off(*args, &block)
+      fetch_without_query_record_off(*args, &block)
     end
   end
   alias_method_chain :fetch, :query_record_off
+end
+
+require 'action_controller/caching/fragments'
+module ActionController::Caching::Fragments
+  def fragment_for_with_query_record_off(*args, &block)
+    ActiveRecord::Base.connection.without_query_record do
+      fragment_for_without_query_record_off(*args, &block)
+    end
+  end
+  alias_method_chain :fragment_for, :query_record_off
 end
