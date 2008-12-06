2aa386560ad301c07718b55bf1024612595946c9
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2008-12-06 13:34:17 +0000

    rake freeze:matthewrudy freezes my gravatar into your public directory

--- lib/you_shouldnt_be_calling_the_database_recursively_in_your_views.rb
+++ lib/you_shouldnt_be_calling_the_database_recursively_in_your_views.rb
@@ -6,10 +6,14 @@ class ActionController::Base
     render_without_db_protection(*args)
     count =  $queries_executed - count_before
     if count > 0
-      render_for_text(%|<div style="position:absolute; top:50px; left:50px; width:800px; height: 400px; background-color: white;"><img src="http://www.gravatar.com/avatar/e60b2dc57668b5662ce3f07781e41710?s=400" style="float:left"/><blockquote style="font-size:40px;">&laquo;You shouldn't be calling the database recursively in your views&raquo;</blockquote></div>|, nil, true)
+      render_for_text(%|<div style="position:absolute; top:50px; left:50px; width:800px; height: 400px; background-color: white;"><img src="#{matthewrudy_url}" style="float:left"/><blockquote style="font-size:40px;">&laquo;You shouldn't be calling the database recursively in your views&raquo;</blockquote></div>|, nil, true)
     end
   end
   alias_method_chain :render, :db_protection
+  
+  def matthewrudy_url
+    File.exist?(File.join(Rails.root, "public", "matthewrudy.jpg")) ? "/matthewrudy.jpg" : "http://www.gravatar.com/avatar/e60b2dc57668b5662ce3f07781e41710?s=400"
+  end
 end
 
 # stolen from rails tests, with an integer count rather than an array, and SHOW TABLES and SHOW FIELDS added to the ignore list
--- tasks/you_shouldnt_be_calling_the_database_recursively_in_your_views_tasks.rake
+++ tasks/you_shouldnt_be_calling_the_database_recursively_in_your_views_tasks.rake
@@ -1,4 +1,10 @@
-# desc "Explaining what the task does"
-# task :you_shouldnt_be_calling_the_database_recursively_in_your_views do
-#   # Task goes here
-# end
+namespace :freeze do
+  task :matthewrudy => :environment do
+    require 'open-uri'
+    gravatar = open("http://www.gravatar.com/avatar/e60b2dc57668b5662ce3f07781e41710?s=400")
+  
+    File.open(File.join(Rails.root, "public", "matthewrudy.jpg"), "w") do |out|
+      out.puts gravatar.read
+    end
+  end
+end
